0bb87a4e52897dc020eff299b2454fe18e86a919
nivinjohn37 <getnivin@gmail.com>
2019-12-27 12:07:04 +0400

    Test

--- Telusko/src/main/java/com/nivin/Telusko/Car.java
+++ Telusko/src/main/java/com/nivin/Telusko/Car.java
@@ -4,7 +4,34 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Car implements Vehicle{
+	private Tyre tyre;
+	
+	/**
+	 * @return the tyre
+	 */
+	public Tyre getTyre() {
+		return tyre;
+	}
+
+
+	/**
+	 * @param tyre the tyre to set
+	 */
+	public void setTyre(Tyre tyre) {
+		this.tyre = tyre;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Car [tyre=" + tyre + "]";
+	}
+
+
 	public void drive() {
-		System.out.println("Car Driving crazy");
+		System.out.println("Test Driving crazy");
 	}
 }
